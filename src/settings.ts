eef5ab063fe83cc41cc2365cc90989c0dfd70a19
cheapproxies <59569096+cheapproxies@users.noreply.github.com>
2020-01-20 12:39:07 +0100

    Update settings.ts

--- src/settings.ts
+++ src/settings.ts
@@ -4,22 +4,22 @@
  This is the link to your domain...it redirects the user to your myaccount page.
  It is used on the site on buttons like: Buy now, Order now, Members, Buy HTTP proxies, etc.
 */
-export const MEMBERS_URL = "https://members.url.goes.here";
+export const MEMBERS_URL = "https://myaccount.cheapproxies.net";
 
 /*
  Your site name - this appears in the footer
 */
-export const SITE_NAME = "Site name goes here";
+export const SITE_NAME = "cheapproxies.net";
 
 /*
  The name of you site that appears in Terms of service section - it can be you domain, your company name, etc.
 */
-export const TOS_SITE_NAME = 'whatever-you-need';
+export const TOS_SITE_NAME = 'cheapproxies.net';
 
 /*
  The date when the terms of service where last updated.
 */
-export const TOS_LAST_UPDATE = 'Apr-27-2018';
+export const TOS_LAST_UPDATE = '01-2020';
 
 /*
  This is the link of your chat widget - it can be found on your tawk to account
